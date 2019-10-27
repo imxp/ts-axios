2a26a503478a8dff2ef1a528b71bbd551484fc0f
xpp <work_xp@126.com>
2019-10-27 18:54:00 +0800

    feat(url.ts util.ts index.ts): 处理请求url参数

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,21 @@
 import { AxiosRequestConfig } from './types'
 import xhr from './xhr'
+import { buildURL } from './helpers/url'
 
 function axios(config: AxiosRequestConfig): void {
+  // 处理config
+  processConfig(config)
   xhr(config)
+}
+
+function processConfig(config: AxiosRequestConfig): void {
+  // 处理url
+  config.url = transformURL(config)
+}
+
+function transformURL(config: AxiosRequestConfig): string {
+  const { url, params } = config
+  return buildURL(url, params)
 }
 
 export default axios
