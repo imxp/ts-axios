91254e9f2016f00499ed537ef05c2b504764ca57
xpp <work_xp@126.com>
2019-10-28 23:36:46 +0800

    feat: axios配置
    axios配置合并 部分

--- src/axios.ts
+++ src/axios.ts
@@ -1,15 +1,17 @@
+import { AxiosRequestConfig } from './types/index'
 import { AxiosInstance } from './types'
 import Axios from './core/Axios'
 import { extend } from './helpers/util'
+import defaults from './defaults'
 
-function createInstance(): AxiosInstance {
-  const context = new Axios()
+function createInstance(config: AxiosRequestConfig): AxiosInstance {
+  const context = new Axios(config)
   const instance = Axios.prototype.request.bind(context)
 
   extend(instance, context)
   return instance as AxiosInstance
 }
 
-const axios = createInstance()
+const axios = createInstance(defaults)
 
 export default axios
