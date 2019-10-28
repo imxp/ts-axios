182a9fda957ba62a511dee952a109bd2a8346b14
xpp <work_xp@126.com>
2019-10-28 11:42:30 +0800

    feat(headers.ts index.ts xhr.ts): promise调用 响应头处理
    返回结果能直接then调用,处理响应头成对象

--- src/xhr.ts
+++ src/xhr.ts
@@ -1,19 +1,45 @@
-import { AxiosRequestConfig } from './types'
+import { AxiosResponse } from './types/index'
+import { AxiosRequestConfig, AxiosPromise } from './types'
+import { paseHeaders } from './helpers/headers'
 
-export default function xhr(config: AxiosRequestConfig) {
-  const { data = null, url, method = 'get', headers } = config
+export default function xhr(config: AxiosRequestConfig): AxiosPromise {
+  return new Promise(resolve => {
+    const { data = null, url, method = 'get', headers, responseType } = config
+    const request = new XMLHttpRequest()
 
-  const request = new XMLHttpRequest()
-
-  request.open(method.toUpperCase(), url, true)
-
-  Object.keys(headers).forEach(name => {
-    if (data === null && name.toLocaleLowerCase() === 'content-type') {
-      delete headers[name]
-    } else {
-      request.setRequestHeader(name, headers[name])
+    if (responseType) {
+      request.responseType = responseType
     }
-  })
 
-  request.send(data)
+    request.open(method.toUpperCase(), url, true)
+
+    request.onreadystatechange = function handleLoad() {
+      if (request.readyState !== 4) {
+        return
+      }
+
+      const responseHeaders = paseHeaders(request.getAllResponseHeaders())
+      const responseData = responseType !== 'text' ? request.response : request.responseText
+      const response: AxiosResponse = {
+        data: responseData,
+        status: request.status,
+        statusText: request.statusText,
+        headers: responseHeaders,
+        config,
+        request
+      }
+      resolve(response)
+    }
+
+    // 设置请求头content-type
+    Object.keys(headers).forEach(name => {
+      if (data === null && name.toLocaleLowerCase() === 'content-type') {
+        delete headers[name]
+      } else {
+        request.setRequestHeader(name, headers[name])
+      }
+    })
+
+    request.send(data)
+  })
 }
