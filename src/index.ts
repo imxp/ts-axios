9ddd828924776c87baa499cc02f38014bfa6adaa
xpp <work_xp@126.com>
2019-10-28 11:58:49 +0800

    feat(index.ts data.tx): 处理相应数据
    对响应数据字符串进行尝试转化为json对象

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
+import { AxiosResponse } from './types/index'
 import { AxiosRequestConfig, AxiosPromise } from './types'
 import xhr from './xhr'
 import { buildURL } from './helpers/url'
-import { transformRequest } from './helpers/data'
+import { transformRequest, transformResponse } from './helpers/data'
 import { processHeaders } from './helpers/headers'
 
 function axios(config: AxiosRequestConfig): AxiosPromise {
   // 处理config
   processConfig(config)
-  return xhr(config)
+  return xhr(config).then(res => {
+    return transformResponseData(res)
+  })
 }
 
 function processConfig(config: AxiosRequestConfig): void {
@@ -31,6 +34,11 @@ function transformRequestData(config: AxiosRequestConfig): any {
 function transformHeaders(config: AxiosRequestConfig): any {
   const { headers = {}, data } = config
   return processHeaders(headers, data)
+}
+
+function transformResponseData(res: AxiosResponse) {
+  res.data = transformResponse(res.data)
+  return res
 }
 
 export default axios
